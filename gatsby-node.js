a7280081b076f4113470cd41f1b9d515823af937
Dan Hayes <danhayes13@gmail.com>
2018-03-14 00:32:35 +0000

    Fixing GraphQL Issue

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,69 +3,92 @@ const path = require("path");
 exports.createPages = ({ boundActionCreators, graphql }) => {
   const { createPage } = boundActionCreators;
 
+  // return graphql(`
+  //   {
+  //     allMarkdownRemark(
+  //       sort: { order: DESC, fields: [frontmatter___date] }
+  //       limit: 1000
+  //     ) {
+  //       edges {
+  //         node {
+  //           excerpt(pruneLength: 400)
+  //           html
+  //           id
+  //           frontmatter {
+  //             templateKey
+  //             path
+  //             date
+  //             title
+  //             image
+  //             heading
+  //             description
+  //             intro {
+  //               blurbs {
+  //                 image
+  //                 text
+  //               }
+  //               heading
+  //               description
+  //             }
+  //             main {
+  //               heading
+  //               description
+  //               image1 {
+  //                 alt
+  //                 image
+  //               }
+  //               image2 {
+  //                 alt
+  //                 image
+  //               }
+  //               image3 {
+  //                 alt
+  //                 image
+  //               }
+  //             }
+  //             testimonials {
+  //               author
+  //               quote
+  //             }
+  //             full_image
+  //             pricing {
+  //               heading
+  //               description
+  //               plans {
+  //                 description
+  //                 items
+  //                 plan
+  //                 price
+  //               }
+  //             }
+  //           }
+  //         }
+  //       }
+  //     }
+  //   }
+  // `)
   return graphql(`
-    {
-      allMarkdownRemark(
-        sort: { order: DESC, fields: [frontmatter___date] }
-        limit: 1000
-      ) {
-        edges {
-          node {
-            excerpt(pruneLength: 400)
-            html
-            id
-            frontmatter {
-              templateKey
-              path
-              date
-              title
-              image
-              heading
-              description
-              intro {
-                blurbs {
-                  image
-                  text
-                }
-                heading
+  {
+    allMarkdownRemark(
+          sort: { order: DESC, fields: [frontmatter___date] }
+          limit: 1000
+        ) {
+          edges {
+            node {
+              excerpt(pruneLength: 400)
+              html
+              id
+              frontmatter {
+                templateKey
+                path
+                date
+                title
                 description
-              }
-              main {
-                heading
-                description
-                image1 {
-                  alt
-                  image
-                }
-                image2 {
-                  alt
-                  image
-                }
-                image3 {
-                  alt
-                  image
-                }
-              }
-              testimonials {
-                author
-                quote
-              }
-              full_image
-              pricing {
-                heading
-                description
-                plans {
-                  description
-                  items
-                  plan
-                  price
-                }
               }
             }
           }
         }
       }
-    }
   `).then(result => {
     if (result.errors) {
       result.errors.forEach(e => console.error(e.toString()));
